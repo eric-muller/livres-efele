a2509a2a4add118422cb179e36f645a126afb953
Eric Muller <eric.muller@efele.net>
2023-12-29 14:14:23 -0800

    La maternelle : fix images

--- livres.publies/frapie/la_maternelle/images/originals/process.sh
+++ livres.publies/frapie/la_maternelle/images/originals/process.sh
@@ -1,19 +1,17 @@
-for f in *.png ; do  
-
-   if [[ "$f" == "h*" ]]; then
-     contrast=0x30
-   else
-     contrast=0x80
-   fi   
+for f in f* p*  ; do  
 
   echo -- $f   
 
  magick.exe $f \
    -separate -evaluate-sequence Mean \
-   -brightness-contrast ${contrast} \
+   -brightness-contrast 0x80 \
    -bordercolor white -border 1x1 \
    -fuzz 20% -trim +repage \
    -transparent white \
    ../$f
 
+done
+
+for f in c* h* ; do
+  cp $f ../$f
 done
