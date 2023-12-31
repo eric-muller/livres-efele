95a947b87dd56a053ee7ece2e0368119618eb4a7
Eric Muller <eric.muller@efele.net>
2023-12-31 09:44:13 -0800

    cosmetic

--- tools/hyphens.sh
+++ tools/hyphens.sh
@@ -1,4 +1,9 @@
- function xslt () {   java -cp "${SAXON_JAR}" net.sf.saxon.Transform -s:$1 -xsl:$2; }
+function xslt () {   
+  java -cp "${SAXON_JAR}" net.sf.saxon.Transform -s:$1 -xsl:$2;
+}
 
-for f in livres.publies/*/*/*.bml;do  xslt $f tools/hyphens.xsl; done | sort > ~/hyphens-efele.txt
+for f in livres.publies/*/*/*.bml; do \
+   xslt $f tools/hyphens.xsl; \
+done \
+| sort > ~/hyphens-efele.txt
 
